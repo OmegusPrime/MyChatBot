acbe313d7f5b1e53dfbf42d4c9f41d6a4b827206
Omegus_Prime <dhruvnandsantosh007@gmail.com>
2026-02-15 15:39:07 +0530

    Fixed error in  Datapipelines Class

--- src/main/backend/Basic Utils/Datapipelines.py
+++ src/main/backend/Basic Utils/Datapipelines.py
@@ -1,7 +1,7 @@
 import FileLoader
 class Datapipelines:
     def __inti__(self, dir):
-        self.loader = FileLoader("D:\MyChatBot\Dataset")
+        self.loader = FileLoader(dir)
     def run(self):
         texts = self.loader.load_all()
         return texts
